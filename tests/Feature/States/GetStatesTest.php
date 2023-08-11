6b33f0d1f64b45768083bb517cbea23f21adcdd9
csc32 <sanzonettycar@gmail.com>
2023-08-11 12:38:33 -0400

    test: add missing tests

--- tests/Feature/States/GetStatesTest.php
+++ tests/Feature/States/GetStatesTest.php
@@ -55,12 +55,31 @@ class GetStatesTest extends TestCase
 
     /** @test */
 
+    public function should_return_404_if_not_find_a_state(): void
+    {
+        $states = States::factory()->count(5)->create();
+
+        $expectedJson = [
+            "message" => "There was a problem to retrieve all information of the state 100",
+            "errors" => [
+                "title" => "Not Found",
+                "code" => 404,
+                "details" => "The is not information of the state with id: 100"
+            ]
+        ];
+
+        $response = $this->getJson($this->url . "/100");
+
+        $response->assertNotFound()->assertJson($expectedJson);
+    }
+    /** @test */
+
     public function should_return_all_states_and_status_200(): void
     {
         $states = States::factory()->count(5)->create();
 
         $expectedJson = [
-            "States" => ['*' => [
+            "states" => ['*' => [
                 "id",
                 "name",
                 "population",
@@ -70,8 +89,25 @@ class GetStatesTest extends TestCase
 
         $response = $this->getJson($this->url);
 
-        $response->assertOk()->assertJsonStructure($expectedJson)->assertJson([
-            "States"
-        ]);
+        $response->assertOk()->assertJsonStructure($expectedJson);
+    }
+    /** @test */
+
+    public function should_return_information_if_state_exist(): void
+    {
+        $states = States::factory()->count(5)->create();
+
+        $expectedJson = [
+            "state" =>  [
+                "id" => $states[0]->id,
+                "name" =>  $states[0]->name,
+                "population" =>  $states[0]->population,
+                "country_id" =>  $states[0]->country_id
+            ]
+        ];
+
+        $response = $this->getJson($this->url . "/" . $states[0]->id);
+
+        $response->assertOk()->assertJson($expectedJson);
     }
 }
