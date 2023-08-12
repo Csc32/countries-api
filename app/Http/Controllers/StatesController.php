f336ce6c08077863611c0f1935f5ac9446e42c4b
csc32 <sanzonettycar@gmail.com>
2023-08-12 19:30:17 -0400

    feat: add validation for update states

--- app/Http/Controllers/StatesController.php
+++ app/Http/Controllers/StatesController.php
@@ -143,9 +143,64 @@ class StatesController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request,  $id): JsonResponse
     {
         //
+        $errorResponse = [
+            "message" => "There was a problem to update the state",
+            "errors" => [
+                "title" => "Bad Request",
+                "code" => 400,
+                "details" => "There are missing parameters"
+            ]
+        ];
+        $typeOfDetail = [
+            "id" => "The parameter should be a number",
+            "name" => "Name should be a string",
+            "population" =>  "Population should be integer",
+            "country_id" =>  "Not exists a country with id " . $request->country_id
+        ];
+
+        $response = [
+            "message" => "State updated correctly"
+        ];
+
+        if (!is_numeric($id)) {
+            $errorResponse['errors']['details'] = $typeOfDetail['id'];
+            return response()->json($errorResponse, 400);
+        }
+        $foundState = States::query()->find($id);
+
+        if (!$foundState) {
+            $errorResponse['errors']['title'] = "Not Found";
+            $errorResponse['errors']['details'] = "The state doesn't exist";
+            $errorResponse['errors']['code'] = 404;
+            return response()->json($errorResponse, 404);
+            # code...
+        }
+
+        foreach ($request->input() as $key => $value) {
+            if (isset($key) && $key == "id" || $key == "name" && is_numeric($value) || $key == 'population' && is_string($value)) {
+                $errorResponse["errors"]['details'] = $typeOfDetail[$key];
+                return response()->json(
+                    $errorResponse,
+                    400
+                );
+            }
+        }
+
+        $country = Countries::query()->find($request->country_id);
+        if (!isset($country)) {
+            $errorResponse["errors"]['details'] = $typeOfDetail['country_id'];
+            return response()->json(
+                $errorResponse,
+                400
+            );
+        }
+
+        if ($foundState->update($request->input())) {
+            return response()->json($response, 200);
+        }
     }
 
     /**
